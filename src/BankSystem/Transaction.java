018a98d7989f88b45a9542c09409adbc6477bb1d
Jen1415 <qiwen894@gmail.com>
2026-07-12 23:23:16 +0800

    Build Transaction class

--- src/BankSystem/Transaction.java
+++ src/BankSystem/Transaction.java
@@ -1,5 +1,37 @@
 package BankSystem;
 
+import java.sql.Timestamp;
+
 public class Transaction {
-	public Transaction() {
+	//enumerator
+	public enum TransactionType { Deposit, Withdrawal };
+
+	//variables
+	private TransactionType type;
+	private double amount;	
+	private String note;
+	private Timestamp localDate;
+	
+	//<<constructors>>
+	// without note
+	public Transaction(TransactionType type, double amount) {
+			this(type, amount, null);
+	}
+
+	// with note
+	public Transaction(TransactionType type, double amount, String note) {
+			this.type = type;
+			this.amount = amount;
+			this.note = note;
+			this.localDate = new Timestamp(System.currentTimeMillis());
+	}
+
+	//toString method
+	@Override
+	public String toString() {
+		return String.format(
+			"type = %s, amount = %.2f, note = %s, LocalDate = %s",
+			type, amount, note, localDate
+		);
+	}
 }
