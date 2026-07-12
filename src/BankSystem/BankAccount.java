3a4c5d983d68c40b06a759d0d2f7cc856aeeb760
wz-Tan <wenzhe1423@gmail.com>
2026-07-12 19:57:52 +0800

    feat: transaction class barebones

--- src/BankSystem/BankAccount.java
+++ src/BankSystem/BankAccount.java
@@ -1,7 +1,14 @@
 package BankSystem;
 
-// Parent Class of Fixed Deposit Account and Savings Account
+import java.util.ArrayList;
+
+// Parent Class of Savings Account and Loan Repayment Account
 public class BankAccount{
+	private String accountNumber; 
+	private double balance; 
+	private final double interestRate = 0.01;  // Set interest rate at 1% per month as constant
+	private ArrayList<Transaction> transactions; // List of transactions under this account
+	
     public BankAccount(){};
     
     void calculateInterest() {
