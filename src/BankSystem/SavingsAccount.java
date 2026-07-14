46ea569ad6337f06a1af295103848223e2973af9
darrenleeee <darren50031@gmail.com>
2026-07-14 09:23:37 +0800

    feat: added savingsaccoutn

--- src/BankSystem/SavingsAccount.java
+++ src/BankSystem/SavingsAccount.java
@@ -1,5 +1,52 @@
 package BankSystem;
 
-public class SavingsAccount {
+import java.time.LocalDate;
 
-}
+public class SavingsAccount extends BankAccount{
+	
+	//declare min balance as rm100 using final and double due to decimal places in money
+	private static final double minBalance = 100.00;
+	
+	
+	//constructors
+	public SavingsAccount () {
+		
+	}
+	
+	//double check if the balance inside savings account is enough
+	boolean verifyMinBalance(double amount) {
+		return ( balance - amount ) >= minBalance;
+	}
+	
+	
+	//withdraw from savings
+	@Override
+	 boolean withdraw(double amount, String note, LocalDate timestamp) {
+	
+		// deny invalid withdrawal amounts
+		if( amount <= 0 ) {
+			System.out.println("INVALID, PLEASE TRY AGAIN");
+			return false;
+		}
+		
+		//check if user is allowed to perform withdraw base on amount in acc
+		if (!verifyMinBalance(amount)) {
+			System.out.println("WITHDRAWAL DENIED. MINIMUM BALANCE OF RM100 IN ACCOUNT IS REQUIRED");
+			return false;
+		}
+		
+		//perform withdraw
+			balance -= amount;
+		
+		//record the transaction performed
+			transactions.add(new Transaction (
+				Transaction.TransactionType.Withdrawal,
+				amount,
+				note,
+				timestamp)
+				);
+			
+		return true;
+		}
+
+	}
