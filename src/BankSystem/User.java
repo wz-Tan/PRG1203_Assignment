1b01707a454165cab637cfc0ea3ea56bccda465c
joeow06 <joeow06@gmail.com>
2026-07-17 22:00:34 +0800

    refactor: moved createAccount functions to User

--- src/BankSystem/User.java
+++ src/BankSystem/User.java
@@ -12,10 +12,10 @@ public class User {
     private String username;
     private int passwordHash; // Compare hashed result
     private ArrayList<BankAccount> accounts = new ArrayList<>(); // ArrayList of User's Bank Accounts
-    
+
     private LocalDate currentDate; // Current Date for User -> Used to log transactions and keep their timing separate
-    private int monthsPassed; 
-    
+    private int monthsPassed;
+
 
     public User() {}
 
@@ -27,7 +27,7 @@ public class User {
         // Assign Params as Fields
         this.username = username;
         this.passwordHash = password.hashCode();
-        this.currentDate = LocalDate.now(); 
+        this.currentDate = LocalDate.now();
         this.monthsPassed = 0;
     }
 
@@ -41,8 +41,8 @@ public class User {
         // Return A Copy (Not Original Reference)
         return new ArrayList<BankAccount>(accounts);
     }
-    
-    // Get Date for Transactions 
+
+    // Get Date for Transactions
     LocalDate getCurrentDate() {
     	return currentDate.plusMonths(monthsPassed);
     }
@@ -56,11 +56,11 @@ public class User {
     boolean verifyPassword(String password) {
         return password.hashCode() == passwordHash;
     }
-    
+
     // Logic to Advance Month (Calculates Interest and Updates currentDate
     void advanceMonth() {
-    	monthsPassed ++; 
-    	
+    	monthsPassed ++;
+
     	for (BankAccount account: accounts) {
     		account.calculateInterest(getCurrentDate());
     	}
@@ -70,7 +70,7 @@ public class User {
     	BankAccount chosenAccount = chooseAccount(scanner);
     	chosenAccount.printAccountInfo();
     }
-    
+
     // Select an Account from All Accounts
     BankAccount chooseAccount(Scanner scanner) {
     	// Return back if no accounts available
@@ -78,38 +78,75 @@ public class User {
     		System.out.println("No accounts available.");
     		return null;
     	}
-    	
+
     	// Prints available accounts
 		int i = 1;
-    	System.out.println("Which account would you like to enquire: ");
+    	System.out.println("Which account would you like to choose: ");
     	for (BankAccount a : accounts) {
-    		System.out.printf("%d. %s\n", i, a.getAccountNumber());
+    		System.out.printf("%d. %s (%s Account)\n", i, a.getAccountNumber(), a.getAccountType());
     		i++;
     	}
-    	
+
     	try {
     		int choice = scanner.nextInt();
         	return accounts.get(choice-1);
     	}
-    	
+
     	catch(InputMismatchException e) {
     		System.out.println("Error: please input a number.");
     		scanner.nextLine();
     	}
-    	
+
     	catch(IllegalArgumentException e) {
     		System.out.println(e.getMessage());
     	}
-    	
+
     	catch(Exception e) {
     		System.out.println("Account does not exist.");
     	}
-    	
+
     	return null;
+    }
+
+    public void createSavingsAccount(Scanner scanner) {
+    	try {
+        	System.out.println("To open a savings account, you need to have at least RM100. How much money would you like to put in?");
+    	    double initialDeposit = scanner.nextDouble();
+    	    scanner.nextLine();
+
+    	    SavingsAccount sa = new SavingsAccount(initialDeposit);
+    	    // Add account to arrayList
+    	    addAccount(sa);
+    	    System.out.println("Savings account created successfully");
+    	}
+    	catch(InputMismatchException e) {
+    		System.out.println("Error: please input a number.");
+    	}
+    	catch(IllegalArgumentException e) {
+    		System.out.println(e.getMessage());
+    	}
+    }
+
+    public void createLoanRepaymentAccount(Scanner scanner) {
+		try {
+    		System.out.print("Enter amount of loan you have (RM):  ");
+    		double outstandingLoan = scanner.nextDouble();
+    		scanner.nextLine();
+    		
+    		LoanRepaymentAccount lra = new LoanRepaymentAccount(outstandingLoan);
+    		addAccount(lra);
+        	System.out.printf("Loan Repayment Account created. Outstanding loan: RM %.2f%n", outstandingLoan);
+		}
+		catch(InputMismatchException e) {
+    		System.out.println("Error: please input a number.");
+    		scanner.nextLine();
+    	}
+    	catch(IllegalArgumentException e) {
+    		System.out.println(e.getMessage());
+    	}
     }
 
     public String toString(){
         return String.format("Username: %s , Password Hash: %s", username, passwordHash);
     }
 }
-
