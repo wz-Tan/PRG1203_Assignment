917d0690203237dd3d69aca7c1c1a3d0fdfb8ec5
joeow06 <joeow06@gmail.com>
2026-07-17 21:57:42 +0800

    refactor: savingsAccount refacotr -  same changes as loanrepayment

--- src/BankSystem/SavingsAccount.java
+++ src/BankSystem/SavingsAccount.java
@@ -6,59 +6,55 @@ import java.util.UUID;
 import java.time.LocalDate;
 
 public class SavingsAccount extends BankAccount{
-	
+
 	//declare min balance as rm100 using final and double due to decimal places in money
 	private static final double minBalance = 100.00;
-	
-	
+
+
 	//constructors
 	public SavingsAccount () {
-		
+		this(0.0);
 	}
-	
+
 	public SavingsAccount(double initialAmount) {
-		balance = initialAmount;
-		type = AccountType.Savings;
+		super(initialAmount, AccountType.Savings);
 	}
-	
+
 	//double check if the balance inside savings account is enough
 	boolean verifyMinBalance(double amount) {
-		return ( balance - amount ) >= minBalance;
+		return ( getBalance() - amount ) >= minBalance;
 	}
-	
-	
+
+
 	//withdraw from savings
 	@Override
 	 boolean withdraw(double amount, String note, LocalDate timestamp) {
-	
+
 		// deny invalid withdrawal amounts
 		if( amount <= 0 ) {
 			System.out.println("INVALID, PLEASE TRY AGAIN");
 			return false;
 		}
-		
+
 		//check if user is allowed to perform withdraw base on amount in acc
 		if (!verifyMinBalance(amount)) {
 			System.out.println("WITHDRAWAL DENIED. MINIMUM BALANCE OF RM100 IN ACCOUNT IS REQUIRED");
 			return false;
 		}
-		
-		//perform withdraw
-			balance -= amount;
-		
-		//record the transaction performed
-			transactions.add(new Transaction(Transaction.TransactionType.Withdrawal, amount, note, timestamp));
-			
+
+		//perform withdraw and record the transaction performed
+			applyWithdrawal(amount, note, timestamp);
+
 			return true;
 	}
-	
+
 	@Override
 	void printAccountInfo() {
-		System.out.printf("Account Number: %s\n", accountNumber);
-		System.out.printf("Account Type: %s\n", type);
-		System.out.printf("Balance       : RM %.2f\n", balance);
+		System.out.printf("Account Number: %s\n", getAccountNumber());
+		System.out.printf("Account Type: %s\n", getAccountType());
+		System.out.printf("Balance       : RM %.2f\n", getBalance());
 		System.out.printf("Transactions: \n");
-		for (Transaction t : transactions) {
+		for (Transaction t : getTransactions()) {
 			System.out.printf("%s\n", t.toString());
 		}
 	}
